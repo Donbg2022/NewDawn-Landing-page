f0f3c3601ba871dddfbe912551be8073af6b6f46
Donovan <donovangriffin04@gmail.com>
2022-11-01 09:45:02 -0700

    large redesign including adding a firebase backend

---
+++ src/app2.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('#burger')
+const linkId = document.querySelector('.mobilelinkid')
+const responsiveNav = document.querySelector('.responsivenav')
+burger.addEventListener('click', function(e){
+  if(getComputedStyle(responsiveNav).display === 'none'){
+  responsiveNav.style.display = 'block'
+}else{
+  responsiveNav.style.display = 'none'
+}
+})
+
+window.addEventListener('resize', (e) => {
+    if (window.innerWidth < 799){
+    responsiveNav.style.display = 'none'
+  }
+})  
+
